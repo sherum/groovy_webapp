1ca4adcf11ffdd8a3aa00c93fdb57128d670e9e8
davidwoolery <sheurm@gmail.com>
2023-09-04 17:59:58 -0500

    baseline structure only

---
+++ groovy-webapp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { StoryComponent } from './components/story/story.component';
+import { StoryListComponent } from './components/story-list/story-list.component';
+import {SharedModule} from "./shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ProfileComponent,
+    AboutComponent,
+    HomeComponent,
+    StoryComponent,
+    StoryListComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
