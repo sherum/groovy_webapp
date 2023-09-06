80dd6cdb8db7b078eba4d71e5cf96010790ac3e6
davidwoolery <sheurm@gmail.com>
2023-09-06 14:17:34 -0500

    add update functionality add httpinterceptor

---
+++ groovy-webapp/src/app/services/user-token.interceptor.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {StoryService} from "./story.service";
+
+@Injectable()
+export class UserTokenInterceptor implements HttpInterceptor {
+
+  constructor(private storyService: StoryService) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log(`UserTokenInterceptor - ${request.url}`);
+
+    let req: HttpRequest<any> =request.clone({
+      setHeaders: {'X-User-Token':"danforth@gmail.com"}
+    });
+  return next.handle(req);
+  }
+}
