31151577b994868ca85ea005356d5f8c0f757d8e
davidwoolery <sheurm@gmail.com>
2023-09-12 16:12:28 -0500

    clean up comments

--- groovy-webapp/src/app/services/plot.service.ts
+++ groovy-webapp/src/app/services/plot.service.ts
@@ -5,7 +5,7 @@ import {StoryService} from "./story.service";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PlotService {
 
@@ -15,59 +15,58 @@ export class PlotService {
   headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/json'});
 
 
-  constructor(private http:HttpClient,private storyService: StoryService) {
-    }
-    selctedStory$ = this.storyService.currentStoryObserver$;
+  constructor(private http: HttpClient, private storyService: StoryService) {
+  }
+
+  selctedStory$ = this.storyService.currentStoryObserver$;
 
 
-    private selectedStoryPlot = new Subject<IPlot>();
-    selectedPlots$ = this.selectedStoryPlot.asObservable();
+  private selectedStoryPlot = new Subject<IPlot>();
+  selectedPlots$ = this.selectedStoryPlot.asObservable();
 
-    selectPlot(plot: IPlot): void {
-        this.selectedStoryPlot.next(plot);
-        console.log("A new plot was chosen",plot);
-    }
+  selectPlot(plot: IPlot): void {
+    this.selectedStoryPlot.next(plot);
+    console.log("A new plot was chosen", plot);
+  }
 
   /**
    * Not sure why I think I need this.
    * @private
    */
   private selectedPlotList = new Subject<IPlot[]>();
-    currentPlotList$ = this.selectedPlotList.asObservable()
+  currentPlotList$ = this.selectedPlotList.asObservable()
 
-    updatePlotList(plot:IPlot[]):void{
-      this.selectedPlotList.next(plot);
-    }
-
-    private storySavePlot = new Subject<IPlot>();
-    insertedSavePlot$ = this.storySavePlot.asObservable();
-
-    updateSavePlot(plot:IPlot):void{
-      this.storySavePlot.next(plot);
-    }
-
-  newPlot(story:IStory): Observable<IPlot> {
-    let storyId = story.id;
-    console.log("New PLot CHeking storyId ",storyId);
-    let payload= "AAA"+storyId;
-    let uri= `${this.getPlotsUri}/new`;
-    return this.http.post<IPlot>(uri, payload,
-      {headers:this.headers});
+  updatePlotList(plot: IPlot[]): void {
+    this.selectedPlotList.next(plot);
   }
 
-  deletePlot(storyId:string,plotId:string):void {
+  private storySavePlot = new Subject<IPlot>();
+  insertedSavePlot$ = this.storySavePlot.asObservable();
 
-      // let sid = "";
-      // let pid = "";
-      // this.selectedPlots$.subscribe(data =>{ pid = <string>data.id});
-      // this.selctedStory$.subscribe(data => {sid = <string>data.id});
-      let uri = `${this.getPlotsUri}/${storyId}/${plotId}`;
-      console.log("The URI: ",uri)
-      this.http.delete(uri,{headers:this.headers}).subscribe(
-        data =>{ console.log("Inside the http delete")},
-        error => console.log("Delete error",error),
-        ()=> console.log("Completed Delete")
-      );
+  updateSavePlot(plot: IPlot): void {
+    this.storySavePlot.next(plot);
+  }
 
-    }
+  newPlot(story: IStory): Observable<IPlot> {
+    let storyId = story.id;
+    console.log("New PLot CHeking storyId ", storyId);
+    let payload = "AAA" + storyId;
+    let uri = `${this.getPlotsUri}/new`;
+    return this.http.post<IPlot>(uri, payload,
+      {headers: this.headers});
+  }
+
+  deletePlot(storyId: string, plotId: string): void {
+
+    let uri = `${this.getPlotsUri}/${storyId}/${plotId}`;
+    console.log("The URI: ", uri)
+    this.http.delete(uri, {headers: this.headers}).subscribe(
+      data => {
+        console.log("Inside the http delete")
+      },
+      error => console.log("Delete error", error),
+      () => console.log("Completed Delete")
+    );
+
+  }
 }
